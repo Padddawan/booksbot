c090d3d9dcdb17242314b10f781de9e5b01f0a84
Padddawan <54628202+Padddawan@users.noreply.github.com>
2019-08-29 07:39:04 +0200

    Update Wiki.py

--- books/spiders/Wiki.py
+++ books/spiders/Wiki.py
@@ -1,3 +1,5 @@
+import scrapy
+
 class WikiSpider(scrapy.Spider):
     name = "wiki_spider"
     start_urls = ['https://wiki.guildwars2.com/wiki/War_Room_Restoration_1']
@@ -6,10 +8,10 @@ class WikiSpider(scrapy.Spider):
         SET_SELECTOR = '.mediawiki'
         for wikiset in response.css(SET_SELECTOR):
 
-            #IMAGE_SELECTOR = 'img ::attr(src)'
+            IMAGE_SELECTOR = 'img ::attr(src)'
             BUILDING_SELECTOR = './/dl[dt/text() = " Building"]/dd/a/text()'
             yield {
                 'building': wikiset.xpath(BUILDING_SELECTOR).extract_first(),
-                #'image': brickset.css(IMAGE_SELECTOR).extract_first(),
-            }
+                'image': brickset.css(IMAGE_SELECTOR).extract_first(),
+}
             
